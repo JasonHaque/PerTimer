d0832201799f693e6f09b5a13a083f5625ba89ba
JasonHaque <zahinjason220434@gmail.com>
2020-04-28 09:24:33 +0600

    added new activity

---
+++ Android Projects/app/src/main/java/com/example/pertimer/authentication/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pertimer.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pertimer.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
